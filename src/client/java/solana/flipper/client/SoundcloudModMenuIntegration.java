18556ff1298630b2a30e44fb4180486e67b67c70
solanaflipper <mikhailkazmerchuk@gmail.com>
2026-05-16 13:37:39 +0200

    Update SoundcloudModMenuIntegration.java

--- src/client/java/solana/flipper/client/SoundcloudModMenuIntegration.java
+++ src/client/java/solana/flipper/client/SoundcloudModMenuIntegration.java
@@ -4,10 +4,7 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import solana.flipper.client.gui.SoundCloudScreen;
 
-/**
- * Registers the SoundCloud player screen as the config screen in Mod Menu.
- * Clicking the gear icon in Mod Menu opens the player directly.
- */
+
 public class SoundcloudModMenuIntegration implements ModMenuApi {
 
     @Override
